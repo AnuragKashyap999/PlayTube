5fbe956181fc2049c88be26d6faf9841e154b128
AnuragKashyap999 <anuragkashyap701153@gmail.com>
2026-07-03 22:55:07 +0530

    add code in header.jsx file

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -1,29 +1,36 @@
 import React from 'react'
 import {useNavigate} from 'react-dom'
+
 function Header() {
     const navigate = useNavigate()
 
     const navItem=[
         {
             title:"Home",
-            path:'"/"'
+            path:"/"
         },
         {
             title:"About",
-            path:'"/about"'
+            path:"/about"
         },
         {
             title:"Sign in" ,
-            path:'"/signup"'
+            path:"/signup"
         },
         {
             title:"Profile",
-            path:'"/profile"'
+            path:"/profile"
         }
     ]
   return (
     <div>
-      
+     <ul>
+      {navItem.map((item)=>(
+        <li key={item.title}>
+            <button onClick={()=>navigate(item.path)}>{item.title}</button>
+        </li>
+      ))}
+     </ul> 
     </div>
   )
 }
